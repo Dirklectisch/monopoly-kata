b435112f52e54a3ac822feacf7ba5f1c504fb574
Dirk Geurs <dirklectisch@mac.com>
2011-01-15 02:40:09 +0100

    Added test for minimum amount of players

--- lib/monopoly/game.rb
+++ lib/monopoly/game.rb
@@ -4,4 +4,5 @@ class Game
   def initialize *players
     @players = *players
   end
+
 end
--- test/tc_game.rb
+++ test/tc_game.rb
@@ -8,5 +8,10 @@ class TestGame < Test::Unit::TestCase
     game = Game.new(player_one, player_two, player_three)
     assert_equal(3, game.players.count)
   end
+  
+  must "Not be able to start a game with less then two players" do
+    only_player = Player.new
+    assert_raise( RuntimeError ) {game = Game.new(only_player)}
+  end
 
 end
