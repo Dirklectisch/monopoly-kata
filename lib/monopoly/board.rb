338408c2d073a580f11f57bb8fc11e40d0675ec5
Dirk Geurs <dirklectisch@mac.com>
2011-01-24 22:44:20 +0100

    Board can calculate relative distance

--- lib/monopoly/board.rb
+++ lib/monopoly/board.rb
@@ -1,9 +1,21 @@
 class Board
-  def initialize positions = []
-    @postions = positions
+  
+  def initialize
+    @postions = 40
   end
   
-  def destination current, distance
-    
+  def start
+    1
   end
+  
+  def destination curr, dist
+    abs = curr + dist
+    if abs < 40 then
+      rel = abs
+    else
+      rel = abs % 40
+    end
+    return rel
+  end
+  
 end
--- test/tc_board.rb
+++ test/tc_board.rb
@@ -5,7 +5,18 @@ class TestBoard < Test::Unit::TestCase
   
   must "Create new game board" do
     a_board = Board.new
-    assert_instance_of(Board, a_board)
+    assert_instance_of Board, a_board
   end
 
+  must "Tell relative destination position" do
+    a_board = Board.new; current = 7;
+    dest = a_board.destination current, 8;
+    assert_equal 15, dest;
+  end
+  
+  must "Must return starting position" do
+    a_board = Board.new
+    assert_equal 1, a_board.start
+  end
+  
 end
