581fc74e5821cf02df59cf0302ea2b9cda1d6cd3
Dirk Geurs <dirklectisch@mac.com>
2011-01-30 04:17:21 +0100

    Rewritten Player tests using minitest/spec

--- test/tc_player.rb
+++ test/tc_player.rb
@@ -1,22 +1,21 @@
-require "unit_extensions"
+require "minitest/autorun"
 require_relative "../lib/monopoly"
 
-class TestPlayer < Test::Unit::TestCase
+describe Player do
   
-  must "Create a player" do
-    anon_player = Player.new
-    assert_instance_of(Player, anon_player)
+  it "must create an anonymous player" do
+    Player.new.must_be_instance_of Player
   end
   
-  must "Indentify a named player" do
-    a_player = Player.new "player_one"
-    assert_equal("player_one", a_player.name)
+  it "must tell a players name" do
+    a_player = Player.new "monopolist"
+    a_player.name.must_equal "monopolist"
   end
   
-  must "Excecute turn movement" do
+  it "must execute turn movement" do
     a_player = Player.new; his_pawn = a_player.pawn;
     a_player.take_turn;
-    assert(his_pawn.position > 1)
-  end 
-  
+    his_pawn.position.wont_equal 1
+  end
 end
+
