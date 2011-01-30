3256e9402368109d9cd597027ad4e5d98cc4acba
Dirk Geurs <dirklectisch@mac.com>
2011-01-30 18:34:16 +0100

    Added save to file option for Position

--- lib/monopoly/position.rb
+++ lib/monopoly/position.rb
@@ -1,3 +1,13 @@
+require 'YAML'
+
+class Position
+  
+  def self.load path
+    YAML.load_file path
+  end
+  
+end
+
 class Position 
   attr_reader :name
   
@@ -5,4 +15,11 @@ class Position
     @name = name
   end
   
+  def save path
+    File.open(path.to_s, 'w') do |out|
+      YAML::dump(self, out)
+    end
+  end
+
 end
+
--- test/tc_position.rb
+++ test/tc_position.rb
@@ -3,10 +3,20 @@ require_relative '../lib/monopoly'
 
 describe Position do
   
-  describe "when a new position is created" do
-    it "should have a name" do
+  describe "when asked to create a new position" do
+    it "must have a name" do
       a_position = Position.new "my name"
       a_position.name.must_equal "my name"
+    end
+    #it "should load a position from preset" do
+    #  a_preset.load
+    #end
+  end
+  
+  describe "when asked to save the position to a file" do
+    it "must save to the specified file" do
+      a_position = Position.new "my name"
+      a_position.save '../data/a_position.yaml'
     end
   end
   
