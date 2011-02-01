8eccaf7ca4c85ff22d0e0d9e596e3ac20e80f51b
Dirk Geurs <dirklectisch@mac.com>
2011-02-01 16:31:51 +0100

    - removed deprecated tests for positions

--- test/tc_position.rb
+++ test/tc_position.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require_relative '../lib/monopoly'
+require 'monopoly/position'
 
 describe Position do
   
@@ -7,16 +7,6 @@ describe Position do
     it "must have a name" do
       a_position = Position.new "my name"
       a_position.name.must_equal "my name"
-    end
-    #it "should load a position from preset" do
-    #  a_preset.load
-    #end
-  end
-  
-  describe "when asked to save the position to a file" do
-    it "must save to the specified file" do
-      a_position = Position.new "my name"
-      a_position.save '../data/a_position.yaml'
     end
   end
   
