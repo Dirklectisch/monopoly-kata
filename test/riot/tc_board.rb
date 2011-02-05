08ddd4f4f17b58960b2b7eee321698fb56c4266d
Dirk Geurs <dirklectisch@mac.com>
2011-02-05 21:56:04 +0100

    - Removed unnecessary tests

--- test/riot/tc_board.rb
+++ test/riot/tc_board.rb
@@ -3,23 +3,17 @@ require 'monopoly/board'
 require 'monopoly/player'
 
 context Board do
-  
-  context "using the default UK positions" do
-    setup { Board.create }
-    asserts(:positions).size(40)
-    asserts("name of second position") {topic.position(2).name}.equals("Old Kent Road")
-    context "put a player on start position" do
-      helper(:a_player) { Player.new }
-      setup { topic.place(:a_player) }
-      asserts("correct start position is found") { topic.name }.equals("Go")
-      asserts("player is at start position") { topic.includes? :a_player}
-    end
-    context ", move a player five places forward." do
-      helper(:a_player) { Player.new }
-      hookup { topic.place(:a_player) }
-      setup  { topic.move(:a_player, 5)}
-      asserts("player position") {topic.to_i}.equals(6)
-    end
+  setup { Board }
+  asserts("it initializes a new board") {topic.new 'some_positions'}.kind_of?(Board)
+  asserts("it creates a board with preset positions") {topic.create}.kind_of?(Board)
+end
+
+context "A board loaded with default positions preset" do
+  helper(:a_player) { Player.new }
+  setup { Board.create }
+  asserts("a player can be placed on start position") {topic.place(:a_player).name == "Go"}
+  context "and a player on start" do
+    hookup { topic.place(:a_player) }
+    asserts("it moves a player forward five places") {topic.move(:a_player, 5).to_i == 6}
   end
-  
 end
