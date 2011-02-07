41ce49baaa2db7efb6159c90b18ae696ac7da1cb
Dirk Geurs <dirklectisch@mac.com>
2011-02-07 22:12:16 +0100

    + Translated some old test cases to riot format'

--- test/riot/tc_game.rb
+++ test/riot/tc_game.rb
@@ -5,5 +5,15 @@ context Game do
   setup { Game }
   asserts("it intializes a new game") { topic.new('player_one', 'player_two') }.is_a?(Game)
   asserts("intializing a single player game") { topic.new('only_player') }.raises (ArgumentError)   
-  #asserts("intializing a nine player game") { topic.new('one', 'two', 'three', 'four', 'five',) }.raises (ArgumentError)   
+  asserts("intializing a nine player game") do
+    topic.new('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+  end.raises (ArgumentError)
+  asserts("In a hunderd two player games, the number of play orders that occurs") do  
+    play_order = []
+    100.times do
+      a_game = Game.new 'Horse', 'Car'
+      play_order << a_game.players.to_s
+    end
+    play_order.uniq.count
+  end.equals 2
 end
