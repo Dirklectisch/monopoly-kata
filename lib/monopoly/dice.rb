cbe0809fa77077577a2130f1b0c0903cde45b488
Dirk Geurs <dirklectisch@mac.com>
2011-02-08 18:15:40 +0100

    - Reduced Dice to the bare minimal'

--- lib/monopoly/dice.rb
+++ lib/monopoly/dice.rb
@@ -1,12 +1,7 @@
 class Dice
-  # A set of two six sided dice
-  attr_reader :amount
-  
-  def initialize
-    @amount = 2
-  end
 
   def roll
     2 + rand(6)
-  end
-end
+  end # Roll the dice
+  
+end # A set of two six sided dice
