60a28d0bce6091fe2f21f7dcf25b25bd8e454a1b
Dirk Geurs <dirklectisch@mac.com>
2011-02-17 22:01:33 +0100

    + Added Board to Game contstructor

--- lib/monopoly/models/game.rb
+++ lib/monopoly/models/game.rb
@@ -6,15 +6,16 @@ module Monopoly
     class Game
       def Game.create *names
         new_players = Player.batch_create(names)
-        self.new *new_players
+        self.new *new_players, Board.create
       end
       
-      attr_reader :players
+      attr_reader :players, :board
 
-      def initialize *players
+      def initialize *players, board
         raise ArgumentError, "Not enough players" if players.count < 2
         raise ArgumentError, "Too many players" if players.count > 8
         self.players = players
+        @board = board
       end # Intialize a new game
 
       private
